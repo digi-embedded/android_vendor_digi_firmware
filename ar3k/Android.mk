d7c3fcd5f84c4d3ec88cb06cf553560f965d1921
Javier Viguera <javier.viguera@digi.com>
2018-11-12 12:34:09 +0100

    ar3k: install firmware under vendor partition
    While on it:
    
      * rename the RamPatch LOCAL_MODULE
      * use BOARD_HAVE_BLUETOOTH_AR3K variable to install or not the
        firmware.
    
    https://jira.digi.com/browse/ADK4A-1393
    
    Signed-off-by: Javier Viguera <javier.viguera@digi.com>

--- ar3k/Android.mk
+++ ar3k/Android.mk
@@ -1,4 +1,4 @@
-ifeq ($(BOARD_USE_AR3K_DIGI_BLUETOOTH),true)
+ifeq ($(BOARD_HAVE_BLUETOOTH_AR3K),true)
 
 LOCAL_PATH:= $(call my-dir)
 define include-ar3k-prebuilt
@@ -18,10 +18,10 @@ define add-ar3k-prebuilt-file
 endef
 
 # Digi AR3002 firmware
-ar3k_hw1020200_dst_dir := $(TARGET_OUT)/etc/firmware/ar3k/1020200
+ar3k_hw1020200_dst_dir := $(TARGET_OUT_VENDOR)/lib/firmware/ar3k/1020200
 $(call add-ar3k-prebuilt-file,1020200/PS_ASIC_class_1.pst,$(ar3k_hw1020200_dst_dir),PS_ASIC_class_1.pst,PS_ASIC_class_1)
 $(call add-ar3k-prebuilt-file,1020200/PS_ASIC_class_2.pst,$(ar3k_hw1020200_dst_dir),PS_ASIC_class_2.pst,PS_ASIC_class_2)
-$(call add-ar3k-prebuilt-file,1020200/RamPatch.txt,$(ar3k_hw1020200_dst_dir),RamPatch.txt,Digi_RamPatch)
+$(call add-ar3k-prebuilt-file,1020200/RamPatch.txt,$(ar3k_hw1020200_dst_dir),RamPatch.txt,RamPatch)
 
 ar3k_hw1020200_dst_dir :=
 
