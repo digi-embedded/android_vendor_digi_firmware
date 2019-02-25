ec3aaa95c74aaf0d8594c250b9512b159689240b
Tatiana Leon <tatiana.leon@digi.com>
2019-02-25 14:08:27 +0100

    qca6574: include proprietary WiFi FW files for qca6574
    This commit adds the current firmware binary files from dey-2.6 (meta-digi repo,
    SHA1 c6a1dbe80c485d8458f4addfa44057b09ea20045).
    
    """
    bf43bd88209b8d7539878c580f8d2d5a  fakeboar.bin
    9d1cf03473904bd8b97fa30bc92b4a16  otp.bin
    b94b58ee95ae8891623833af4d684216  qwlan30.bin
    d0b35881991978824c60716733282760  utf.bin
    """
    
    https://jira.digi.com/browse/ADK4A-1585
    
    Signed-off-by: Isaac Hermida <isaac.hermida@digi.com>
    Signed-off-by: Tatiana Leon <tatiana.leon@digi.com>

---
+++ qca6574/Android.mk
@@ -0,0 +1,42 @@
+ifeq ($(BOARD_HAVE_WIFI_QCA6574),true)
+
+LOCAL_PATH:= $(call my-dir)
+define include-qca6574-prebuilt
+    include $$(CLEAR_VARS)
+    LOCAL_MODULE := $(4)
+    LOCAL_MODULE_STEM := $(3)
+    LOCAL_MODULE_TAGS := debug eng optional
+    LOCAL_MODULE_CLASS := ETC
+    LOCAL_MODULE_PATH := $(2)
+    LOCAL_SRC_FILES := $(1)
+    ALL_DEFAULT_INSTALLED_MODULES += $(LOCAL_MODULE)
+    include $$(BUILD_PREBUILT)
+endef
+
+define add-qca6574-prebuilt-file
+    $(eval $(include-qca6574-prebuilt))
+endef
+
+qca6574_dst_dir := $(TARGET_OUT_VENDOR)/firmware
+
+$(call add-qca6574-prebuilt-file,qca6574_proprietary/qwlan30.bin,$(qca6574_dst_dir),qwlan30.bin,qwlan30)
+$(call add-qca6574-prebuilt-file,qca6574_proprietary/fakeboar.bin,$(qca6574_dst_dir),fakeboar.bin,fakeboar)
+$(call add-qca6574-prebuilt-file,qca6574_proprietary/otp.bin,$(qca6574_dst_dir),otp.bin,otp)
+$(call add-qca6574-prebuilt-file,qca6574_proprietary/utf.bin,$(qca6574_dst_dir),utf.bin,utf)
+
+define add-qca6574-symlink
+$(1):
+	@echo "Symlink: $$@ -> $(2)"
+	@mkdir -p $$(dir $$@)
+	@rm -f $$@
+	$$(hide) ln -sf $(2) $$@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(1)
+endef
+
+$(eval $(call add-qca6574-symlink,$(qca6574_dst_dir)/athwlan.bin,qwlan30.bin))
+$(eval $(call add-qca6574-symlink,$(qca6574_dst_dir)/athsetup.bin,otp.bin))
+
+qca6574_dst_dir :=
+
+endif
