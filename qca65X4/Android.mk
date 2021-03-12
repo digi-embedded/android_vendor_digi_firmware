952531409f6fafecdf08ee6b526807c5341c3d9e
Javier Viguera <javier.viguera@digi.com>
2021-03-12 18:51:49 +0100

    qca65X4: update firmware files for new version of QCACLD driver
    Copied from DEY as of tag dey-3.0-r3.2 (bc843e239d4ccad86f6ceb61714ce2474ec7f00f)
    
    100a982b33b4a2cfeb34cdea794dc6d6  qca65X4/qca65X4_proprietary/pci/bdwlan30_US.bin
    9d1cf03473904bd8b97fa30bc92b4a16  qca65X4/qca65X4_proprietary/pci/otp30.bin
    41cfb4e50613cd0eeb0fa99a005131bd  qca65X4/qca65X4_proprietary/pci/utf.bin
    794f7a6f12ef3e6f8db6257b3a6f896f  qca65X4/qca65X4_proprietary/pci/qwlan30.bin
    
    f9600f7bf4d601494b9bbe1ccbe94d6a  qca65X4/qca65X4_proprietary/sdio/bdwlan30_US.bin
    4743dee015047752e433e69f4db89974  qca65X4/qca65X4_proprietary/sdio/utf30.bin
    e018e7b629b055dc4616c7e88c615b41  qca65X4/qca65X4_proprietary/sdio/otp30.bin
    fe37bc043132ecf21e504eb33f46b846  qca65X4/qca65X4_proprietary/sdio/qwlan30.bin
    
    Copied PCI and SDIO version of the firmware, as the CC8X uses PCI, while
    the CC8M Mini uses SDIO.
    
    https://jira.digi.com/browse/ADK4A-1803
    
    Signed-off-by: Javier Viguera <javier.viguera@digi.com>

--- qca65X4/Android.mk
+++ qca65X4/Android.mk
@@ -5,11 +5,10 @@ define include-qca65X4-prebuilt
     include $$(CLEAR_VARS)
     LOCAL_MODULE := $(4)
     LOCAL_MODULE_STEM := $(3)
-    LOCAL_MODULE_TAGS := debug eng optional
+    LOCAL_MODULE_TAGS := optional
     LOCAL_MODULE_CLASS := ETC
     LOCAL_MODULE_PATH := $(2)
     LOCAL_SRC_FILES := $(1)
-    ALL_DEFAULT_INSTALLED_MODULES += $(LOCAL_MODULE)
     include $$(BUILD_PREBUILT)
 endef
 
@@ -20,10 +19,20 @@ endef
 qca65X4_dst_dir := $(TARGET_OUT_VENDOR)/firmware
 
 # Wi-Fi firmware
-$(call add-qca65X4-prebuilt-file,qca65X4_proprietary/qwlan30.bin,$(qca65X4_dst_dir),qwlan30.bin,qwlan30)
-$(call add-qca65X4-prebuilt-file,qca65X4_proprietary/fakeboar.bin,$(qca65X4_dst_dir),fakeboar.bin,fakeboar)
-$(call add-qca65X4-prebuilt-file,qca65X4_proprietary/otp.bin,$(qca65X4_dst_dir),otp.bin,otp)
-$(call add-qca65X4-prebuilt-file,qca65X4_proprietary/utf.bin,$(qca65X4_dst_dir),utf.bin,utf)
+ifeq ($(QCACLD_WIFI_INTERFACE), sdio)
+$(call add-qca65X4-prebuilt-file,qca65X4_proprietary/sdio/qwlan30.bin,$(qca65X4_dst_dir),qwlan30.bin,qwlan30)
+$(call add-qca65X4-prebuilt-file,qca65X4_proprietary/sdio/bdwlan30_US.bin,$(qca65X4_dst_dir),bdwlan30.bin,bdwlan30)
+$(call add-qca65X4-prebuilt-file,qca65X4_proprietary/sdio/otp30.bin,$(qca65X4_dst_dir),otp30.bin,otp30)
+$(call add-qca65X4-prebuilt-file,qca65X4_proprietary/sdio/utf30.bin,$(qca65X4_dst_dir),utf30.bin,utf30)
+else ifeq ($(QCACLD_WIFI_INTERFACE), pci)
+$(call add-qca65X4-prebuilt-file,qca65X4_proprietary/pci/qwlan30.bin,$(qca65X4_dst_dir),qwlan30.bin,qwlan30)
+$(call add-qca65X4-prebuilt-file,qca65X4_proprietary/pci/bdwlan30_US.bin,$(qca65X4_dst_dir),bdwlan30.bin,bdwlan30)
+$(call add-qca65X4-prebuilt-file,qca65X4_proprietary/pci/otp30.bin,$(qca65X4_dst_dir),otp30.bin,otp30)
+$(call add-qca65X4-prebuilt-file,qca65X4_proprietary/pci/utf.bin,$(qca65X4_dst_dir),utf.bin,utf)
+else
+$(error Variable QCACLD_WIFI_INTERFACE not set.)
+endif
+
 # Bluetooth firmware
 $(call add-qca65X4-prebuilt-file,qca65X4_bt/nvm_tlv_3.2.bin,$(qca65X4_dst_dir),nvm_tlv_3.2.bin,nvm_tlv)
 $(call add-qca65X4-prebuilt-file,qca65X4_bt/rampatch_tlv_3.2.tlv,$(qca65X4_dst_dir),rampatch_tlv_3.2.tlv,rampatch_tlv)
@@ -34,16 +43,31 @@ $(1):
 	@mkdir -p $$(dir $$@)
 	@rm -f $$@
 	$$(hide) ln -sf $(2) $$@
-
-ALL_DEFAULT_INSTALLED_MODULES += $(1)
 endef
 
+FW_SYMLINKS_LIST := \
+    $(qca65X4_dst_dir)/utfbd30.bin \
+    $(qca65X4_dst_dir)/wlan/wlan_mac0 \
+    $(qca65X4_dst_dir)/wlan/wlan_mac1 \
+    $(qca65X4_dst_dir)/wlan/wlan_mac2 \
+    $(qca65X4_dst_dir)/wlan/wlan_mac3
+
+ifeq ($(QCACLD_WIFI_INTERFACE), pci)
+FW_SYMLINKS_LIST += \
+    $(qca65X4_dst_dir)/athsetup.bin \
+    $(qca65X4_dst_dir)/athwlan.bin
+endif
+
 $(eval $(call add-qca65X4-symlink,$(qca65X4_dst_dir)/athwlan.bin,qwlan30.bin))
-$(eval $(call add-qca65X4-symlink,$(qca65X4_dst_dir)/athsetup.bin,otp.bin))
+$(eval $(call add-qca65X4-symlink,$(qca65X4_dst_dir)/athsetup.bin,otp30.bin))
+$(eval $(call add-qca65X4-symlink,$(qca65X4_dst_dir)/utfbd30.bin,bdwlan30.bin))
 $(eval $(call add-qca65X4-symlink,$(qca65X4_dst_dir)/wlan/wlan_mac0,/proc/device-tree/wireless/mac-address))
 $(eval $(call add-qca65X4-symlink,$(qca65X4_dst_dir)/wlan/wlan_mac1,/proc/device-tree/wireless/mac-address1))
 $(eval $(call add-qca65X4-symlink,$(qca65X4_dst_dir)/wlan/wlan_mac2,/proc/device-tree/wireless/mac-address2))
 $(eval $(call add-qca65X4-symlink,$(qca65X4_dst_dir)/wlan/wlan_mac3,/proc/device-tree/wireless/mac-address3))
+
+# Force a dependence from the firmware so the symlinks are created
+$(qca65X4_dst_dir)/qwlan30.bin: $(FW_SYMLINKS_LIST)
 
 qca65X4_dst_dir :=
 
